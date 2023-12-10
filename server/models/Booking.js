6104cb50a306135ceda06d0c538b13a8b8f48301
Gabien21 <pktoan21@clc.fitus.edu.vn>
2023-12-10 15:45:41 +0700

    Update booking schema

--- server/models/Booking.js
+++ server/models/Booking.js
@@ -1,29 +1,58 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+// const BookingSchema = new Schema({
+//   user: {
+//     type: Schema.Types.ObjectId,
+//     ref: "users",
+//   },
+//   table_type: {
+//     type: Schema.Types.ObjectId,
+//     ref: "tables",
+//   },
+//   table_date: {
+//     type: Date,
+//   },
+//   room_type: {
+//     type: Schema.Types.ObjectId,
+//     ref: "rooms",
+//   },
+//   room_date: {
+//     start_room_date: {
+//       type: Date,
+//     },
+//     end_room_date: {
+//       type: Date,
+//     },
+//   },
+//   pay: {
+//     type: Number,
+//     required: true
+//   },
+//   state: {
+//     type: String,
+//     enum: ['true', 'false'],
+//     default: 'false',
+//     required: true
+//   }
+// });
+
 const BookingSchema = new Schema({
   user: {
     type: Schema.Types.ObjectId,
     ref: "users",
   },
+  table_booking: {
+    type: Schema.Types.ObjectId,
+    ref: "bookingtable",
+  },
+  room_booking: {
+    type: Schema.Types.ObjectId,
+    ref: "bookingroom",
+  },
   table_type: {
     type: Schema.Types.ObjectId,
     ref: "tables",
-  },
-  table_date: {
-    type: Date,
-  },
-  room_type: {
-    type: Schema.Types.ObjectId,
-    ref: "rooms",
-  },
-  room_date: {
-    start_room_date: {
-      type: Date,
-    },
-    end_room_date: {
-      type: Date,
-    },
   },
   pay: {
     type: Number,
