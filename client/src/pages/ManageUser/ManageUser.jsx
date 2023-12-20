588de16ea7f9ac42b76c6b131321144c6a38ecba
Namiscrea7or <pphnam21@clc.fitus.edu.vn>
2023-12-20 16:43:59 +0700

    push again

--- client/src/pages/ManageUser/ManageUser.jsx
+++ client/src/pages/ManageUser/ManageUser.jsx
@@ -2,9 +2,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import GuestDetails from '../../components/guest detail/GuestDetails';
+import ReactPaginate from 'react-paginate';
+import './user.css';
 
 const ManageUser = () => {
   const [guests, setGuests] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0); // Changed from 1 to 0
+  const [itemsPerPage] = useState(2); // Change this value to set the number of items per page
 
   useEffect(() => {
     const fetchData = async () => {
@@ -22,38 +26,48 @@ const ManageUser = () => {
     fetchData();
   }, []);
 
+  const indexOfLastItem = (currentPage + 1) * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentGuests = guests.slice(indexOfFirstItem, indexOfLastItem);
 
+  const handlePageClick = ({ selected }) => {
+    setCurrentPage(selected);
+  };
 
   const handleDeleteGuest = async (guestId) => {
     try {
-      console.log(guestId)
+      console.log(guestId);
       const res = await axios.delete(`http://localhost:5000/api/user/${guestId}`, {
         headers: {
-          Authorization: localStorage.getItem('Saved Token')
-        }
+          Authorization: localStorage.getItem('Saved Token'),
+        },
       });
 
-      console.log(localStorage.getItem('Saved Token'))
-      console.log(res.data)
-
+      console.log(localStorage.getItem('Saved Token'));
+      console.log(res.data);
     } catch (e) {
       console.log('Wrong details');
     }
   };
 
   return (
-    <div>
+    <div className='ManageUser'>
       <h2>Manage Guests</h2>
       <ul>
-        {guests.map((guest) => (
+        {currentGuests.map((guest) => (
           <li key={guest.email}>
-            <GuestDetails
-              guest={guest}
-              onDeleteClick={handleDeleteGuest}
-            />
+            <GuestDetails guest={guest} onDeleteClick={handleDeleteGuest} />
           </li>
         ))}
       </ul>
+      <ReactPaginate
+        pageCount={Math.ceil(guests.length / itemsPerPage)}
+        pageRangeDisplayed={2}
+        marginPagesDisplayed={1}
+        onPageChange={handlePageClick}
+        containerClassName={'pagination'}
+        activeClassName={'active'}
+      />
     </div>
   );
 };
