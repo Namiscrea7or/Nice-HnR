0cbb632a9123a8c3a1c4274a9580cf2496bca0ee
thanhthao00 <nttthao392003@gmail.com>
2023-12-21 00:11:25 +0700

    change Feedback page interface

--- client/src/pages/Feedback/Feedback.jsx
+++ client/src/pages/Feedback/Feedback.jsx
@@ -40,10 +40,10 @@ const Feedback = () => {
           const { full_name, address, birthday, user_id, phone_number, role } = feedback.user || {};
           return (
             <div key={index} className="feedback-item">
-              <img src={`https://robohash.org/${user_id}`} alt='photo'/>
-              <p>User: {full_name}</p>
-              <p>Description: {feedback.description}</p>
+              <img className="user-img" src={`https://robohash.org/${user_id}`} alt='photo'/>
+              <div className="user-name">{full_name}</div>
               <div className="star">{renderStars(feedback.rate)}</div>
+              <div className="user-cmt">"{feedback.description}"</div>
             </div>
           );
         })}
